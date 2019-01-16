1026dda96e208952acf7d0e25ddf9367d9e7f99d
161250167 <1158873864@qq.com>
2019-01-16 17:20:33 +0800

    fix

--- web/config/dev.env.js
+++ web/config/dev.env.js
@@ -5,5 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://10.107.30.218:8080"',
+  //BASE_API: '"http://10.107.30.218:8080"',
+  BASE_API: '"https://junrongcenter.com:8080"',
 })
--- web/config/prod.env.js
+++ web/config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://10.107.30.218:8080"',
+  //BASE_API: '"http://10.107.30.218:8080"',
+  BASE_API: '"https://junrongcenter.com:8080"',
 }
